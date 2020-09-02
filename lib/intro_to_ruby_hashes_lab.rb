9b3eb5fdbf479b971d7bf09578fd24a64650507f
Inee Ader <xoliio@gmail.com>
2020-09-02 17:38:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
 def my_hash_creator(key, value)
   my_hash = {
-    :key => value 
+    :key => "value"
   }
   my_hash
   # return a hash that includes the key and value parameters passed into this method
