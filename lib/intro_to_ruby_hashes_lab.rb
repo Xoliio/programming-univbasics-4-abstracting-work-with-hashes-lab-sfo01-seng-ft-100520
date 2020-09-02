68b58c9ac8dfec3b332f864a8acefe72ae0a2a52
Inee Ader <xoliio@gmail.com>
2020-09-02 17:54:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,8 +3,6 @@ def my_hash_creator(key, value)
     :key => value
   }
   my_hash
-
-  # return a hash that includes the key and value parameters passed into this method
 end
 
 def read_from_hash(hash, key)
